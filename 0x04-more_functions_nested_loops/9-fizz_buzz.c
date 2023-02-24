f6eab3825a80e451f8c63f85ac32ab2fa7e1c382
moatlaletlhogonolo <moatlaletlhogonolo@gmail.com>
2023-02-23 17:21:31 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 	{
 	printf("FizzBuzz");
 	}
-	else if (x % == 0)
+	else if (x % 3 == 0)
 	{
 	printf("Fizz");
 	}
@@ -35,6 +35,6 @@ int main(void)
 	printf(" ");
 	}
 	}
-	_putchar('\n');
+	printf("\n");
 	return (0);
 }
