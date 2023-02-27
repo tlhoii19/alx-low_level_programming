6baa8f49fc77a664580e4fc71f6e1da1005994a1
moatlaletlhogonolo <moatlaletlhogonolo@gmail.com>
2023-02-27 14:10:02 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,13 +15,13 @@ void print_rev(char *s)
 
 	while (s[c] != '\0')
 	{
-	c++
+	c++;
 	}
 
-	for (c -= 1; c >= 0 c--)
+	for (c -= 1; c >= 0; c--)
 	{
 	_putchar(s[c]);
 	}
-	_puchar('\n');
+	_putchar('\n');
 
 }
