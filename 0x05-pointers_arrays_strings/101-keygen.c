db34d27f11480cebcb6b9bd5c230a4d387054757
moatlaletlhogonolo <moatlaletlhogonolo@gmail.com>
2023-02-27 16:49:56 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,13 +11,14 @@
 int main(void)
 
 {
+
 	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
 	srand(time(0));
 
 	while (sum < 2772)
-	
+
 	{
 		password[index] = 33 + rand() % 94;
 		sum += password[index++];
@@ -48,8 +49,9 @@ int main(void)
 									password[index] -= diff_half2;
 									
 										break;
-								}
+							}
 						}
 	}
-
+	printf("%s", password);
+	return (0);
 }
