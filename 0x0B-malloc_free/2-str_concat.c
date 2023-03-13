13642abafa5062b0037a5241e9d79194eaaad809
moatlaletlhogonolo <moatlaletlhogonolo@gmail.com>
2023-03-13 10:01:12 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,6 +7,7 @@
  * str_concat - concatenates two strings of any size
  * @s1: the first string to concatenate
  * @s2: the second string to concatenate
+ *
  * Return: the two strings concatenated
  */
 
