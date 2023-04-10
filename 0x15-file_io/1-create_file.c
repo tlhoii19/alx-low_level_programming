d89cf8d2d97a79fd38b99019f8fc1249ae47c52a
“tlhoii19” <moatlaletlhogonolo@gmail.com>
2023-04-10 05:48:02 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * Create_file - Creates a file.
+ * create_file - Creates a file.
  * @filename: the name of the file to create.
  * @text_content: string to write to the file.
  * Return: If the function fails -1.
  *         Otherwise - 1.
  */
+
 int create_file(const char *filename, char *text_content)
 {
 
